b2d0de735f0bd440d9e92dc91532f82870c82d28
yellhtetaung <yairhtet248.yha@gmail.com>
2022-10-06 02:48:47 +0630

    updated

---
+++ 05online/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
+//   function printAge() {
+//     let output = `${firstName} age is ${age} and born in ${birthYear}`;
+
+//     if (birthYear > 1981 && birthYear <= 1996) {
+//       // const age = 46;
+//       const str = `Oh, and you are a lucky person, ${firstName}`;
+//       console.log(str);
+//       var luckyPerson = true;
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       output = 'Hello World Rename Result';
+//       // console.log(age);
+//       console.log(output);
+//     }
+//     console.log(luckyPerson);
+
+//     // console.log(output);
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Hello World';
+// calcAge(1990);
+
+// console.log(fullName); // undefined
+// console.log(job); // referenceError
+// console.log(birthYear); // referenceError
+
+// var fullName = 'IT Yan Tote';
+// let job = 'Developer';
+// const birthYear = 1990;
+
+// var environment
+// {
+//   fullName: undefined;
+//   job: TDZ;
+//   birthYear: TDZ;
+//   addExp: undefined;
+// }
+
+// console.log(addDecl(2, 3));
+// console.log(addExp(2, 3));
+// console.log(addArr(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// var addExp = function (a, b) {
+//   return a + b;
+// };
+
+// var addArr = (a, b) => a + b;
+
+// var allProducts = 10;
+// if (!allProducts) deleteShoppingCart();
+// function deleteShoppingCart() {
+//   console.log('Delete All Products');
+// }
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(window);
