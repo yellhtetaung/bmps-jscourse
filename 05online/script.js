cbb0f0ebdb16901223431bfa27ee2fed277a794c
yellhtetaung <yairhtet248.yha@gmail.com>
2022-10-06 15:57:50 +0630

    updated

--- 05online/script.js
+++ 05online/script.js
@@ -65,7 +65,104 @@
 //   console.log('Delete All Products');
 // }
 
-var x = 1;
-let y = 2;
-const z = 3;
-console.log(window);
+// var x = 1;
+// let y = 2;
+// const z = 3;
+// console.log(window);
+
+// console.log(this);
+
+// function calcAge(birthYear) {
+//   console.log(2022 - birthYear);
+//   console.log(this);
+// }
+// calcAge(1998);
+
+// const calcAge = birthYear => {
+//   console.log(2022 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1998);
+
+// const mgmg = {
+//   year: 1990,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2022 - this.year);
+//   },
+// };
+// mgmg.calcAge();
+
+// const koko = {
+//   year: 1987,
+// };
+// koko.calcAge = mgmg.calcAge;
+// console.log(koko);
+// koko.calcAge();
+
+// const f = mgmg.calcAge;
+// console.log(f);
+
+// var firstName = 'BMPS Education Center';
+
+// const person1 = {
+//   firstName: 'William',
+//   years: 1990,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2022 - this.years);
+
+//     const self = this;
+
+//     const luckyPerson = () => {
+//       console.log(this.years >= 1980 && this.years <= 1996);
+//     };
+//     luckyPerson();
+//   },
+//   greet: () => console.log(`Hey ${this.firstName}`),
+// };
+
+// person1.greet();
+// person1.calcAge();
+
+// const add = function (a, b) {
+//   console.log(arguments);
+//   console.log(a + b);
+// };
+
+// add(2, 3, 4, 5);
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(oldAge);
+// console.log(age);
+
+// const me = {
+//   name: 'Hello World',
+//   age: 30,
+// };
+
+// const myFriend = me;
+// myFriend.age = 35;
+// console.log(me);
+// console.log(myFriend);
+
+// let firstName = 'Marry';
+// let lastName = 'Brown';
+// let oldLastName = lastName;
+// lastName = 'David';
+// console.log(oldLastName);
+// console.log(lastName);
+
+const jsMgMg = {
+  firstName: 'JavaScript',
+  lastName: 'Mg Mg',
+};
+
+const pythonMgMg = jsMgMg;
+pythonMgMg.firstName = 'Python';
+console.log(jsMgMg);
+console.log(pythonMgMg);
+
+// jsMgMg = {};
