49f8a08701ee3ca9fa8286a3b988c983d34cbf86
yellhtetaung <yairhtet248.yha@gmail.com>
2022-10-20 17:25:49 +0630

    updated

--- 06online/script.js
+++ 06online/script.js
@@ -309,3 +309,31 @@ const restaurant = {
 // const arr = [1, 2, 3];
 // const [a, b, c] = arr;
 // console.log(a, b, c);
+
+const question = new Map([
+  ['question', 'What is the best programming in the world'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try Again'],
+]);
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = Number(prompt('Please enter you answer'));
+// console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// console.log(question);
+
+// console.log(Object.entries(openingHours));
+// const hourMap = new Map(Object.entries(openingHours));
+// console.log(hourMap);
